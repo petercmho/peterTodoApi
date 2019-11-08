deac6546e7687185c0efd0b36f5187cf28b64ee0
Peter Ho@VNCAL-9JM5RV2 <pho@douglaslightingcontrols.com>
2019-11-07 18:23:20 -0800

    Complete ProductsController implementation and add Key attribute to ProductId in ProductItem model class.

--- PeterTodoApi/Controllers/ProductsController.cs
+++ PeterTodoApi/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PeterTodoApi.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace PeterTodoApi.Controllers
 {
-    public class ProductsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
     {
+        private readonly IProductsService _productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        // GET /api/products
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductsAsync()
+        {
+            return await _productsService.GetAllProductsAsync();
+        }
+
+        // GET /api/products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductAsync(int id)
+        {
+            return await _productsService.GetProductAsync(id);
+        }
+
+        // GET /api/products/find
+        [HttpGet("find/{sku}")]
+        public async Task<IActionResult> FindProductsAsync(string sku)
+        {
+            return await _productsService.FindProductsAsync(sku);
+        }
+
+        // DELETE /api/products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductAsync(int id)
+        {
+            return await _productsService.DeleteProductAsync(id);
+        }
     }
 }
--- PeterTodoApi/Models/ProductItem.cs
+++ PeterTodoApi/Models/ProductItem.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PeterTodoApi.Models
 {
+    //[Table("Products")]
     public class ProductItem
     {
+        [Key]
         public int ProductId { get; set; }
         public string Sku { get; set; }
         public string Name { get; set; }
